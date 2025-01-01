fac62a086080ddef276692df30fe3549b6750f6a
Shreyash patel <shreyashpatel5506@gmail.com>
2025-01-01 20:52:29 +0530

    Rename Privacy page import to Policy in App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Privacy from "./pages/Privacy";
+import Privacy from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
