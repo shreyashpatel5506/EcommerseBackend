ae4646b3d4b5353703f6f3b2ac85fe52e39d43ed
Shreyash patel <shreyashpatel5506@gmail.com>
2025-01-11 19:15:54 +0530

    Add product management features: implement product retrieval, deletion, and update controllers; enhance product routes

--- controller/PrdouctController.js
+++ controller/PrdouctController.js
@@ -1,6 +1,7 @@
 import ProductModel from "../models/ProductModel.js";
 import slugify from "slugify";
 import fs from "fs";
+import Product from "../models/ProductModel.js";
 
 export const addProduct = async (req, res) => {
   try {
@@ -17,6 +18,134 @@ export const addProduct = async (req, res) => {
       });
     }
     const newProduct = new ProductModel({ ...req.fields, slug: slugify(name) });
+    if (imageUrl) {
+      newProduct.imageUrl.data = fs.readFileSync(imageUrl.path);
+      newProduct.contentType = imageUrl.type;
+    }
+    await newProduct.save();
+    res.status(201).send({
+      success: true,
+      message: "Product created successfully",
+      product: newProduct,
+    });
+    console.log(newProduct);
+  } catch (error) {
+    console.log("Error :-" + error);
+    res.status(500).send({
+      success: false,
+      message: "Error during creating a product",
+      error,
+    });
+  }
+};
+
+export const getProductController = async (req, res) => {
+  try {
+    const getProducts = await ProductModel.find({})
+      .select("-imageUrl")
+      .limit(12)
+
+      .sort({ createdAt: -1 });
+    return res.status(200).send({
+      success: true,
+      countTotal: getProducts.length,
+      message: "All Products",
+      getProducts,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Error during getting a product",
+      error,
+    });
+  }
+};
+
+export const getSingleProductController = async (req, res) => {
+  try {
+    const product = await ProductModel.findOne({
+      slug: req.params.slug,
+    }).select("-imageUrl");
+
+    if (!product) {
+      return res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "Product fetched successfully",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Error during getting a product",
+      error,
+    });
+  }
+};
+
+export const fetchProductPhoto = async (req, res) => {
+  try {
+    const ProductPhoto = await ProductModel.findById(req.params.pid).select(
+      "imageUrl"
+    );
+    if (ProductPhoto.imageUrl.data) {
+      res.set("Content-type", ProductPhoto.imageUrl.contentType);
+      return res.status(200).send(ProductPhoto.imageUrl.data);
+    }
+  } catch (error) {
+    console.log("error::" + error);
+    return res.status(500).send({
+      success: false,
+      message: "Error during getting a product image",
+      error,
+    });
+  }
+};
+
+export const DeleteProduct = async (req, res) => {
+  try {
+    const Product = await ProductModel.findOneAndDelete(req.params.pid);
+    res.status(200).send({
+      success: true,
+      message: "Product deleted successfully",
+      Product,
+    });
+  } catch (error) {
+    console.log("error::" + error);
+    return res.status(500).send({
+      success: false,
+      message: "Error during getting a product image",
+      error,
+    });
+  }
+};
+
+export const UpdateProduct = async (req, res) => {
+  try {
+    const { name, description, price, category, stock } = req.fields;
+    const { imageUrl } = req.files;
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(500).send({
+        error: "All feilds are required",
+      });
+    }
+    if (!imageUrl || imageUrl.size > 1000000) {
+      return res.status(500).send({
+        error: "Photo must be less than 10 mb",
+      });
+    }
+    const newProduct = await ProductModel.findByIdAndUpdate(
+      req.params.pid,
+      {
+        ...req.feilds,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
     if (imageUrl) {
       newProduct.imageUrl.data = fs.readFileSync(imageUrl.path);
       newProduct.contentType = imageUrl.type;
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,33 @@
 import express from "express";
 import { fetchuser, isAdmin } from "../midleware/fetchuser.js";
 import foridable from "express-formidable";
-import { addProduct } from "../controller/PrdouctController.js";
+import {
+  addProduct,
+  DeleteProduct,
+  fetchProductPhoto,
+  getProductController,
+  getSingleProductController,
+  UpdateProduct,
+} from "../controller/PrdouctController.js";
 
 const router = express.Router();
 
 router.post("/Create-Product", fetchuser, isAdmin, foridable(), addProduct);
+
+router.get("/get-Products", getProductController);
+
+router.get("/get-SingleProduct/:slug", getSingleProductController);
+
+router.get("/get-ProductPhoto/:pid", fetchProductPhoto);
+
+router.delete("/delete-Product/:pid", DeleteProduct);
+
+router.post(
+  "/Update-Product/:pid",
+  fetchuser,
+  isAdmin,
+  foridable(),
+  UpdateProduct
+);
 
 export default router;
