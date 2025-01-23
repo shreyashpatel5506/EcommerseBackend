24b0338906dbd60aeac0550328c5d6bbab297283
Shreyash patel <shreyashpatel5506@gmail.com>
2025-01-23 20:10:00 +0530

    new

--- client/src/component/Forms/categoryform.js
+++ client/src/component/Forms/categoryform.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CategoryForm = ({ handleSubmit, value, setValue }) => {
+const CategoryForm = () => {
+  const [newCategory, setNewCategory] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log("New Category:", newCategory);
+    // Reset the input field after submission
+    setNewCategory("");
+  };
+
   return (
-    <>
+    <div class="mb-6">
+      <label
+        for="success"
+        class="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
+      >
+        Your name
+      </label>
+      <input
+        type="text"
+        id="success"
+        class="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+        placeholder="Success input"
+        disabled
+      />
+      <p class="mt-2 text-sm text-green-600 dark:text-green-500">
+        <span class="font-medium">Well done!</span> Some success message.
+      </p>
+
       <form onSubmit={handleSubmit}>
         <div className="mb-3 w-75">
           <input
             type="text"
             className="form-control"
             placeholder="Enter new category"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={newCategory}
+            onChange={(e) => setNewCategory(e.target.value)}
           />
         </div>
         <button
@@ -20,7 +47,7 @@ const CategoryForm = ({ handleSubmit, value, setValue }) => {
           Submit
         </button>
       </form>
-    </>
+    </div>
   );
 };
 
