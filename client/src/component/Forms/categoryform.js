d8acdb3f393861935a5dfa891afe7fe727970434
Shreyash patel <shreyashpatel5506@gmail.com>
2025-01-23 22:34:41 +0530

    Refactor CategoryForm component: simplify state management by passing props for handling submission and input value; enhance styling for better user experience.

--- client/src/component/Forms/categoryform.js
+++ client/src/component/Forms/categoryform.js
@@ -1,53 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
-const CategoryForm = () => {
-  const [newCategory, setNewCategory] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission logic here
-    console.log("New Category:", newCategory);
-    // Reset the input field after submission
-    setNewCategory("");
-  };
-
+const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
-    <div class="mb-6">
-      <label
-        for="success"
-        class="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
-      >
-        Your name
-      </label>
-      <input
-        type="text"
-        id="success"
-        class="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
-        placeholder="Success input"
-        disabled
-      />
-      <p class="mt-2 text-sm text-green-600 dark:text-green-500">
-        <span class="font-medium">Well done!</span> Some success message.
-      </p>
-
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3 w-75">
+    <>
+      <div className="mb-6">
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
-            className="form-control"
-            placeholder="Enter new category"
-            value={newCategory}
-            onChange={(e) => setNewCategory(e.target.value)}
+            className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+            placeholder="Enter a new Category"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
           />
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-2 mx-3"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-2 mx-3"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
